766658f898a9347f1f93fa6243fd2a1923727255
Eduard38655 <93397077+Eduard38655@users.noreply.github.com>
2022-11-13 19:37:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Proof(params) {
+	return (
+		<div className="cont-Inde">
+			<img
+				className="img-cont"
+				src={require('../imagen/active-states.jpg')}
+				alt="active-states"
+			/>
+			<div className="con-text-Inde">
+				<p className="nombre-cont">jacob</p>
+				<p className="cont-age">ten</p>
+				<p className="cont-local">santiago</p>
+			</div>
+		</div>
+	);
+}
+export default Proof;
